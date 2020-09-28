f63b4ebc1348ede393f7faea4fc318c5a604fc9a
Gustavo Santos <gustavo.war2015@gmail.com>
2020-09-28 18:11:39 -0300

    Parte A - implementando classe Aluno

---
+++ src/Aluno.kt
@@ -0,0 +1,13 @@
+class Aluno(var nome: String,
+            var sobrenome: String,
+            val codigo: Int){
+
+    override fun equals(other: Any?): Boolean {
+        if(this === other) return true
+        if(other !is Aluno) return false
+
+        if(codigo != other.codigo) return false
+        
+        return true
+    }
+}
