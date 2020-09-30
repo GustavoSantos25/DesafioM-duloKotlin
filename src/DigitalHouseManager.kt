15c80a759611245503a5bd12e95437a3dbae5eeb
Gustavo Santos <gustavo.war2015@gmail.com>
2020-09-30 10:26:56 -0300

    Parte G - implementando classe DigitalHouseManager

---
+++ src/DigitalHouseManager.kt
@@ -0,0 +1,6 @@
+class DigitalHouseManager {
+    val listaAlunos = mutableListOf<Aluno>()
+    val listaCurso = mutableListOf<Curso>()
+    val listaProfessores = mutableListOf<Professor>()
+    val listaMatriculas = mutableListOf<Matricula>()
+}
